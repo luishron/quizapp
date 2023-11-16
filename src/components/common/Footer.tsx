d3e80a3a52a904e4e015071948d9f6a07f105262
Luis H Ron <luishron@gmail.com>
2023-11-16 18:48:36 -0300

    Update Footer component with styled component and new text

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,10 +1,26 @@
+import styled from '@emotion/styled';
 import { Box, Typography } from '@mui/material';
+import { Theme } from '@mui/material/styles';
+import theme from '../../theme/theme';
+
+const FooterSection = styled(Box)(({ theme }: { theme: Theme }) => ({
+  background: theme.palette.background.paper,
+  boxShadow: 'none',
+}));
 
 const Footer = () => {
   return (
-    <Box component="footer" sx={{ padding: 2, textAlign: 'center' }}>
-      <Typography variant="body1">© 2023 Mi Aplicación</Typography>
-    </Box>
+    <FooterSection
+      theme={theme}
+      component="footer"
+      sx={{
+        padding: 2,
+        textAlign: 'start',
+        borderTop: '1px solid rbga(225,225,225,01)',
+      }}
+    >
+      <Typography variant="body1">© 2023 QuizDapp</Typography>
+    </FooterSection>
   );
 };
 
