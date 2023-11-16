5e1d12eaf4d8b80726ef09ae6a23ec45090c2edf
Luis H Ron <luishron@gmail.com>
2023-11-16 18:39:34 -0300

    Refactor Header component styling.

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,15 +1,22 @@
-// src/components/common/Header/Header.tsx
+import styled from '@emotion/styled';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
+import { Theme } from '@mui/material/styles';
+import theme from '../../theme/theme';
 import Logo from './Logo';
+
+const StyledAppBar = styled(AppBar)(({ theme }: { theme: Theme }) => ({
+  background: theme.palette.background.paper,
+  boxShadow: 'none',
+}));
 
 const Header = () => {
   return (
-    <AppBar position="static">
+    <StyledAppBar theme={theme} position="static">
       <Toolbar>
         <Logo />
       </Toolbar>
-    </AppBar>
+    </StyledAppBar>
   );
 };
 
