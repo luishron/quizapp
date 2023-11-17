1f62bfe5de0cac14e7344379d0dc42588b921f25
Luis H Ron <luishron@gmail.com>
2023-11-17 19:16:50 -0300

    Refactor Logo component styling

--- src/components/common/Logo.tsx
+++ src/components/common/Logo.tsx
@@ -1,13 +1,24 @@
-import { Box, Typography } from '@mui/material';
+import styled from '@emotion/styled';
+import { Box, Theme, Typography } from '@mui/material';
 import { FaLayerGroup } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
+import theme from '../../theme/theme';
+
+const StyledLogo = styled(Box)(({ theme }: { theme: Theme }) => ({
+  color: theme.palette.primary.light,
+  display: 'flex',
+  flexDirection: 'row',
+  alignItems: 'center',
+  gap: '0.5em',
+}));
+
 const Logo = () => {
   return (
     <Link to="/" style={{ textDecoration: 'none', color: 'inherit' }}>
-      <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+      <StyledLogo theme={theme}>
         <FaLayerGroup size="24" />
         <Typography variant="h6">QuizDapp</Typography>
-      </Box>
+      </StyledLogo>
     </Link>
   );
 };
