efb5f7dec5ade380f693d44abb552cdf8d975e7c
Luis H Ron <luishron@gmail.com>
2023-11-18 10:42:01 -0300

    Add wallet connection and network switch functionality to SurveyPage

--- src/views/SurveyPage.tsx
+++ src/views/SurveyPage.tsx
@@ -1,10 +1,36 @@
+import { Box, Typography } from '@mui/material';
+import { useAccount, useNetwork } from 'wagmi';
+import ConnectButton from '../components/common/ConnectButton';
+import SwitchNetwork from '../components/common/SwitchNetwork';
 import MainLayout from '../components/layout/MainLayout';
-import SurveyContainer from '../components/survey/SurveyContainer';
+import SurveyStep from '../components/survey/SurveyStep';
 
 const SurveyPage = () => {
+  const { chain } = useNetwork();
+  const { isConnected } = useAccount();
+  const isNetwork = chain?.id === 5;
   return (
     <MainLayout>
-      <SurveyContainer />
+      <>
+        {!isConnected && (
+          <Box sx={{ textAlign: 'center' }}>
+            <Typography variant="subtitle1" component="p">
+              Please connect your wallet
+            </Typography>
+            <ConnectButton />
+          </Box>
+        )}
+
+        {isConnected && !isNetwork && (
+          <Box sx={{ textAlign: 'center' }}>
+            <Typography variant="subtitle1" component="p">
+              Please switch to the Goerli network
+            </Typography>
+            <SwitchNetwork />
+          </Box>
+        )}
+        {isConnected && isNetwork && <SurveyStep />}
+      </>
     </MainLayout>
   );
 };
