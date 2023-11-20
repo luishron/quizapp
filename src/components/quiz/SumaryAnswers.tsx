41b414fba8e008a5746b54c246ef3c2a74049690
Luis H Ron <luishron@gmail.com>
2023-11-20 08:52:18 -0300

    Refactor summary answers layout

--- src/components/quiz/SumaryAnswers.tsx
+++ src/components/quiz/SumaryAnswers.tsx
@@ -28,51 +28,60 @@ const SumaryAnswers = ({ quiz, answers }) => {
   };
   return (
     <Card sx={cardStyle}>
-      <CardContent
+      <Box
         sx={{
           display: 'flex',
           flexDirection: 'column',
-          gap: '1rem',
+          justifyContent: 'center',
+          padding: '1rem',
           width: isMobileOrTablet ? 'auto' : '50%',
           height: isMobileOrTablet ? '100%' : 'auto',
-          border: '1px solid red',
         }}
       >
-        <Typography variant="h4" component="h2" color="primary">
-          Summary of Answers
-        </Typography>
-        {quiz.questions.map((question, index) => (
-          <Box key={index}>
-            <Typography
-              variant="h6"
-              component="h3"
-              color={theme.palette.text.primary}
-            >
-              {question.text}
-            </Typography>
-            <Typography variant="body1" color={theme.palette.text.secondary}>
-              {answers[index] ? answers[index] : 'No answer'}
-            </Typography>
-          </Box>
-        ))}
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={handleSubmit}
-          disabled={!isResult}
+        <CardContent
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            gap: '1rem',
+          }}
         >
-          Send answers
-        </Button>
-      </CardContent>
+          <Typography variant="h4" component="h2" color="primary">
+            Summary of Answers
+          </Typography>
+          {quiz.questions.map((question, index) => (
+            <Box key={index}>
+              <Typography
+                variant="h6"
+                component="h3"
+                color={theme.palette.text.primary}
+              >
+                {question.text}
+              </Typography>
+              <Typography variant="body1" color={theme.palette.text.secondary}>
+                {answers[index] ? answers[index] : 'No answer'}
+              </Typography>
+            </Box>
+          ))}
+          <Box>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={handleSubmit}
+              disabled={!isResult}
+            >
+              Send answers
+            </Button>
+          </Box>
+        </CardContent>
+      </Box>
       <CardMedia
         component="img"
-        alt={quiz.title}
+        alt={`Image for ${quiz.title}`}
         image="./img/answers.png"
         sx={{
           background: 'rgba(0,0,0,0.2)',
           width: isMobileOrTablet ? 'auto' : '50%',
           height: isMobileOrTablet ? '100%' : 'auto',
-          border: '1px solid blue',
         }}
       />
     </Card>
